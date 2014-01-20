b3e9ada19bd43d57415224649249e8e8330d7fe3
Sherzod Odinaev <sherzodinayev@yahoo.com>
2014-01-20 18:11:54 -0500

    minot UI update; refactored test bootstrap

--- tests/unit/bootstrap.php
+++ tests/unit/bootstrap.php
@@ -1,17 +1,5 @@
 <?php
 define('CONFIG_DIR', realpath(__DIR__ . '/config'));
-require __DIR__ . '/../../vendor/autoload.php';
-$paths = array(
-    get_include_path(),
-    __DIR__
-
-);
-set_include_path(join(PATH_SEPARATOR, $paths));
-
-function tester_autoload($classname)
-{
-    $file = str_replace('\\', '/', $classname) . '.php';
-    require_once $file;
-}
-
-spl_autoload_register('tester_autoload');
+$loader = require __DIR__ . '/../../vendor/autoload.php';
+$loader->add('Application', __DIR__);
+$loader->add('Falcon', __DIR__);
